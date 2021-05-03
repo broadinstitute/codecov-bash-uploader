074e9ff2280e6359e64479de04cc335f107b4f3c
Denis Loginov <denis@broadinstitute.org>
2021-05-03 16:08:26 -0400

    Grep SHA${i}SUM files for codecov checksum

--- codecov-verified.bash
+++ codecov-verified.bash
@@ -5,7 +5,7 @@ curl -s https://codecov.io/bash > codecov
 CODECOV_VERSION=$(grep 'VERSION=\".*\"' codecov | cut -d'"' -f2);
 for i in 1 256 512
 do
-  diff <(shasum -a $i codecov) <(curl -s "https://raw.githubusercontent.com/codecov/codecov-bash/${CODECOV_VERSION}/SHA${i}SUM") \
+  diff <(shasum -a $i codecov) <(curl -s "https://raw.githubusercontent.com/codecov/codecov-bash/${CODECOV_VERSION}/SHA${i}SUM" | grep codecov) \
   || { echo "CodeCov checksum validation failed!" >&2; exit 1; }
 done
 bash codecov "$@"
